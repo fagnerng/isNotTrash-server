6125ec4eed6387afbaa197f0204dc9b317aaff2e
Diego Augusto <diego.oliveira@embedded.ufcg.edu.br>
2016-02-29 14:59:52 -0300

    Insere post nas rotas de promotions

--- routes/promotion.js
+++ routes/promotion.js
@@ -6,7 +6,7 @@ var promotionController = require('../controllers/promotionController.js');
 var validator = require('validator');
 
 /*Implementa método GET para recuperar todas as promoções*/
-router.get('/', function(req, res){
+router.post('/', function(req, res){
     var params = url.parse(req.url, true).query;
 
     promotionController.all(req.body.user_id,
@@ -17,7 +17,7 @@ router.get('/', function(req, res){
 });
 
 /*Implementa serviço de requisição de produtos por página e por tamanho de página.*/
-router.get('/morePromotions', function(req, res){
+router.post('/morePromotions', function(req, res){
 
     var skip = parseInt(validator.trim(validator.escape(req.body.skip)));
     var limit = parseInt(validator.trim(validator.escape(req.body.limit)));
@@ -31,7 +31,7 @@ router.get('/morePromotions', function(req, res){
 });
 
 /*Implementa serviço de requisição de produtos por página e por tamanho de página. Fazer acontecer com webScoket*/
-router.get('/newPromotions', function(req, res){
+router.post('/newPromotions', function(req, res){
 
     var first = validator.trim(validator.escape(req.body.first));
     var user_id = validator.trim(validator.escape(req.body.user_id));
