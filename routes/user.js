154db1a35aac6916ec5f28ac2b61c3be0656d0d0
BrunoRafael <bruno.rafael.vasconcelos@ccc.ufcg.edu.br>
2016-03-04 09:20:03 -0300

    refactoring comments

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var userController = require('../controllers/userController.js');
 var validator = require('validator');
+var url = require('url');
 
 /*Rota que retorna todos os usuários*/
 router.get('/', function(req, res) {
@@ -17,7 +18,8 @@ router.get('/', function(req, res) {
 
 /*Rota que retorna o usuário específico*/
 router.get('/:id', function(req, res) {
-    var id = validator.trim(validator.escape(req.param('id')));
+    var json = url.parse(req.url, true).query;
+    var id = validator.trim(validator.escape(json.id));
 
     userController.user(id, function(resp) {
         if(resp.error !== undefined){
@@ -30,10 +32,12 @@ router.get('/:id', function(req, res) {
 
 /*Rota que cria um usuário*/
 router.post('/', function(req, res) {
-    var name = validator.trim(validator.escape(req.param('name')));
-    var email = validator.trim(validator.escape(req.param('email')));
-    var password = validator.trim(validator.escape(req.param('password')));
-    var phone = validator.trim(validator.escape(req.param('phone')));
+    var json = url.parse(req.url, true).query;
+
+    var name = validator.trim(validator.escape(json.name));
+    var email = validator.trim(validator.escape(json.email));
+    var password = validator.trim(validator.escape(json.password));
+    var phone = validator.trim(validator.escape(json.phone));
 
     userController.save(name, email, password,phone, function(resp) {
         if(resp.error !== undefined){
@@ -46,11 +50,13 @@ router.post('/', function(req, res) {
 
 /*Rota que atualiza um usuário*/
 router.put('/', function(req, res) {
-    var id = validator.trim(validator.escape(req.param('id')));
-    var name = validator.trim(validator.escape(req.param('name')));
-    var email = validator.trim(validator.escape(req.param('email')));
-    var password = validator.trim(validator.escape(req.param('password')));
-    var phone = validator.trim(validator.escape(req.param('phone')));
+    var json = url.parse(req.url, true).query;
+
+    var id = validator.trim(validator.escape(json.id));
+    var name = validator.trim(validator.escape(json.name));
+    var email = validator.trim(validator.escape(json.email));
+    var password = validator.trim(validator.escape(json.password));
+    var phone = validator.trim(validator.escape(json.phone));
 
     userController.update(id, name, email, password, phone, function(resp) {
         if(resp.error !== undefined){
@@ -63,7 +69,9 @@ router.put('/', function(req, res) {
 
 /*Rota que deleta um usuário*/
 router.delete('/:id', function(req, res) {
-    var id = validator.trim(validator.escape(req.param('id')));
+    var json = url.parse(req.url, true).query;
+
+    var id = validator.trim(validator.escape(json.id));
     userController.delete(id, function(resp) {
         if(resp.error !== undefined){
             res.send(resp, 404);
